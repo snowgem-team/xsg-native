ad7e4258ff07742a3bfc84428c2bac3c34f0ad03
Jan Vrastil <vrastil.honza@gmail.com>
2020-11-16 07:57:24 +0100

    wip

--- src/@ionic-native/plugins/sum-sub/index.ts
+++ src/@ionic-native/plugins/sum-sub/index.ts
@@ -24,18 +24,23 @@ import { Plugin, Cordova, IonicNativePlugin } from '@ionic-native/core';
  * ```
  */
 @Plugin({
-  pluginName: 'SumSub',
-  plugin: 'cordova-sum-sub', // npm package name, example: cordova-plugin-camera
+  pluginName: 'SNSMobileSdkCordovaPlugin',
+  plugin: 'SNSMobileSdkCordovaPlugin', // npm package name, example: cordova-plugin-camera
   pluginRef: 'cordova.plugins.sumSub', // the variable reference to call the plugin, example: navigator.geolocation
   repo: 'https://github.com/expcapitaldev/sumsub-cordova-idensic-mobile-sdk-plugin', // the github repository URL for the plugin
   install: 'ionic cordova plugin add @sumsub/cordova-idensic-mobile-sdk-plugin', // OPTIONAL install command, in case the plugin requires variables
-  platforms: ['Android', 'iOS'] // Array of platforms supported, example: ['Android', 'iOS']
+  platforms: ['Android', 'iOS'], // Array of platforms supported, example: ['Android', 'iOS']
 })
 @Injectable()
 export class SumSub extends IonicNativePlugin {
-
   @Cordova()
-  launchSNSMobileSDK(apiUrl: string, flowName: string, accessToken: string, locale: string, supportEmail: string): Promise<any> {
+  launchSNSMobileSDK(params: {
+    apiUrl: string;
+    flowName: string;
+    accessToken: string;
+    locale: string;
+    supportEmail: string;
+  }): Promise<any> {
     return; // We add return; here to avoid any IDE / Compiler errors
   }
 
